059ad56c164a3fd296e2ede73a9330572082e013
Naveed786Ahmed <138322439+Naveed786Ahmed@users.noreply.github.com>
2023-10-15 12:03:43 +0500

    Initial commit

---
+++ alert.js
@@ -0,0 +1,98 @@
+//Chapter No: 01 > Question No: 01
+alert("Hello Javasript");
+
+//Chapter No: 01 > Question No: 02
+// alert("Error! Please Enter Your Valid Password");
+
+//Chapter No: 01 > Question No: 03
+// alert("Welcome To JS Land... \n Happy Coding!")
+
+//Chapter No: 01 > Question No: 04
+// alert("Welcome To JS Land...")
+// alert("Happy Coding! \n Prevent this page from creating additional dialogs.")
+
+//Chapter No: 01 > Question No: 05
+// alert("Hello.. I can run JS through my web browser's console")
+
+//Chapter No: 02 > Question No: 01
+// var Username;
+
+//Chapter No: 02 > Question No: 02
+// var myName = "Naveed Ahmed";
+
+//Chapter No: 02 > Question No: 03
+// var message = "Hello World";
+// alert(message);
+
+//Chapter No: 02 > Question No: 04
+// var name = "Naveed Ahmed";
+// var age = "15 year old";
+// var course = "Certified Mobile Application Development";
+
+// alert(name);
+// alert(age);
+// alert(course);
+
+//Chapter No: 02 > Question No: 05
+// var name = "Pizza";
+// document.write(name);
+// alert(name);
+// var name = "Pizz";
+// alert(name);
+// var name = "Piz";
+// alert(name);
+// var name = "Pi";
+// alert(name);
+// var name = "P";
+// alert(name);
+
+//Chapter No: 02 > Question No: 06
+// var email = "example@example.com";
+// alert("My email address is" + " " + email);
+
+//Chapter No: 02 > Question No: 07
+// var book = "A smarter way to learn Javascript";
+// alert("I am trying to learn from the Book" + " " + book);
+
+//Chapter No: 02 > Question No: 08
+// document.write("Yah! I can write HTML content through Javascript");
+
+
+//Chapter No: 03 > Question No: 01
+// var age = 20;
+// alert("I am" + " " + age + " " + " Year Old");
+
+//Chapter No: 03 > Question No: 02
+// var N = 14;
+// alert("How Many Times a visitor has visited a web page?")
+// alert("You have visited the site" + " " + N + " " + "times");
+
+//Chapter No: 03 > Question No: 03
+// var birthYear = 2002;
+// document.write("My Birth Year is" + " " + birthYear);
+
+//Chapter No: 03 > Question No: 04
+// var visitorName = "Naveed Ahmed";
+// var productTitle = "T-shirts";
+// var quantity = 5;
+// document.write(visitorName + " " + "ordered" + " " + quantity + " " + productTitle + " " + "on XYZ Clothing store");
+
+//Chapter No: 04 > Question No: 01
+// var name = "Naveed Ahmed";
+// var firstName = "Naveed";
+// var lastName = "Ahmed";
+// console.log(name)
+// console.log(firstName)
+// console.log(lastName)
+
+//Chapter No: 04 > Question No: 03
+// document.write("<h1> Rules for naming JS variables </h1>");
+// document.write("Variable names can only contain , numbers, $ and For example: $my_1stVariable. <br>");
+// document.write("Variable must begin with a letter, $ or. For example: $name, _name or name. <br>");
+// document.write("Variable names are case sensitive. <br>");
+// document.write("Variable names should not be JS Keywords.");
+
+
+
+
+
